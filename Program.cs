bc1db1b31d8d22758cbfd86ada40f4a082769010
mar6le <124502106+mar6le@users.noreply.github.com>
2024-05-27 14:30:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+using Magazine.Data;
+using Magazine.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Segment.Model;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MagazineContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "GGG"));
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<MagazineContext>();
+
+
+// Add services to the container.
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+   
+    
+builder.Services.AddScoped(sp => ShoppingCart.GetCart(sp));
+
+
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+ 
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    
+}
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<MagazineContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.MapRazorPages();
+app.Run();
